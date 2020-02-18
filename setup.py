5ce8e93b33d2fe8aa908a30abdaf4f4d27d75dcd
jeremy-Stafford <jeremyspianopenguin@gmail.com>
2020-02-18 13:28:44 +0000

    Finalise `setup.py`

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ from setuptools import find_packages, setup
 
 from datacli import __version__
 
+with open("README.md") as readme:
+    long_description = readme.read()
+
 setup(
     name="dataclass-cli",
     version=__version__,
-    packages=find_packages(),
+    packages=find_packages(exclude=("tests",)),
 
     author="Jeremy Stafford",
     author_email="jeremyspianopenguin@gmail.com",
     description="A library for building simple CLIs from dataclasses",
-    keywords="dataclass cli",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Jeremy-Stafford/datacli",
-    classifies=[
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
