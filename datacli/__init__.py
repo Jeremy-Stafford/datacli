46f25e4a24fb519774973032669d78450388095f
Simon Ertl <simon.ertl@willhaben.at>
2021-05-07 13:31:45 +0200

    add option to supply environment variables and enforce them

--- datacli/__init__.py
+++ datacli/__init__.py
@@ -1,3 +1,6 @@
+import os
+from typing import List, Optional, Tuple
+
 """A library for building simple CLIs from dataclasses.
 
 DataCLI is based on argparse.
@@ -21,18 +24,48 @@ def get_names(field):
         yield "--" + field.name.replace("_", "-")
 
 
+def from_env_var(env_var_name: str):
+    """add a default factory to extract the cli argument from env"""
+    def _get() -> str:
+        return os.getenv(env_var_name, f"")
+    return _get
+
+
 def make_parser(cls):
     """Create an argument parser from a dataclass."""
     parser = ArgumentParser()
 
     for field in fields(cls):
+        corresponding_env_var = field.metadata.get("env_var")
+
         required = (field.default is MISSING
                     and field.default_factory is MISSING)
+
+        env_var_default: Optional[str] = None
+        help_text = field.metadata.get("help")
+
+        if corresponding_env_var:
+            required = False
+            help_text = help_text + f", can also be set with environment variable {corresponding_env_var}"
+            env_var_default = os.getenv(corresponding_env_var, "")
+
         arg_type = field.metadata.get("arg_type", field.type)
+
         parser.add_argument(*get_names(field),
-                            type=arg_type, required=required)
+                            type=arg_type, # type: ignore
+                            help=help_text,
+                            required=required,
+                            default=env_var_default)
 
     return parser
+
+def check_fields_with_env_defaults(instance):
+    for field in fields(type(instance)):
+        corresponding_env_var = field.metadata.get("env_var")
+        if corresponding_env_var and getattr(instance, field.name) == "":
+            error_message = f"{field.name} not set, either supply arguments {list(get_names(field))} " \
+                + f"or set environment variable {corresponding_env_var}"
+            raise ValueError(error_message)
 
 
 def datacli(cls, argv=None):
@@ -40,4 +73,6 @@ def datacli(cls, argv=None):
     parser = make_parser(cls)
     data = {key: val for key, val in vars(parser.parse_args(argv)).items()
             if val is not None}
-    return cls(**data)
+    instance = cls(**data)
+    check_fields_with_env_defaults(instance)
+    return instance
